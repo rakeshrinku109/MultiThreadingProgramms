5436856814b8eef66ed7a0bfdf23c926565fb784
RakeshPadhi <rakesh.rinku109@gmail.com>
2018-04-04 18:02:45 +0530

    Add files via upload

---
+++ Polymorphism.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpFundametals
+{
+
+    class Parent
+    {
+
+        public Parent()
+        {
+            Console.WriteLine("Parent class constructor called");
+        }
+        internal virtual void Write()
+        {
+            Console.WriteLine("parent class write method called");
+        }
+    }
+
+    class child : Parent
+    {
+        public int MyProperty { get; set; }
+        public child()
+        {
+            Console.WriteLine("child class constructor called");
+        }
+
+        internal sealed override void Write()
+        {
+            Console.WriteLine("child class write method called");
+        }
+
+    }
+
+    class GrandChild : child
+    {
+        public GrandChild()
+        {
+            Console.WriteLine("child class constructor called");
+        }
+
+        internal new void Write()
+        {
+            Console.WriteLine("grand child class write method called");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpFundametals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            #region PolyMorphism client
+
+            //GrandChild gc = new GrandChild();
+            //child c = gc;
+            //c.Write(); 
+            #endregion
+        }
+    }
+
+}
